446321d7f143faf2faff692ca60af92f6013f88e
Danilo Cestari <danilo@compucorp.co.uk>
2016-06-03 18:53:38 +0200

    PCHR-1082: Initial style overrides

--- gulpfile.js
+++ gulpfile.js
@@ -4,17 +4,27 @@ var rename = require('gulp-rename');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function () {
-  gulp.src('scss/*.scss')
+  gulp.src('scss/style.scss')
     .pipe(bulk())
     .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
     .pipe(rename(function (path) {
-        path.basename = 'bootstrap-civicrm-' + path.basename;
+      path.basename = 'bootstrap-civicrm-' + path.basename;
+    }))
+    .pipe(gulp.dest('css/'));
+});
+
+gulp.task('global-sass', function () {
+  gulp.src('scss/global-theme-overrides/global-custom.scss')
+    .pipe(bulk())
+    .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
+    .pipe(rename(function (path) {
+      path.basename = 'global-custom';
     }))
     .pipe(gulp.dest('css/'));
 });
 
 gulp.task('watch', function () {
-    gulp.watch('scss/**/*.scss', ['sass']);
+  gulp.watch('scss/**/*.scss', ['sass']);
 });
 
-gulp.task('default', ['sass', 'watch']);;
+gulp.task('default', ['sass', 'watch']);
