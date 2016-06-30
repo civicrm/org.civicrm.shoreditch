e8182d5b79ca85233f47808fe5c7845c162e68c1
Danilo Cestari <danilo@compucorp.co.uk>
2016-06-30 13:23:59 +0200

    PCHR-1082: PR tweaks

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,33 @@
-require('es6-promise').polyfill();
 var gulp = require('gulp');
 var bulk = require('gulp-sass-bulk-import');
-var rename = require('gulp-rename');
 var sass = require('gulp-sass');
 var postcss = require('gulp-postcss');
+// The "postcss-prefix-selector" uses ES6 promises
+require('es6-promise').polyfill();
 // Used to add necessary prefixes at the css level
 var postcssPrefix = require('postcss-prefix-selector');
 // Used to remove duplicated css rules
 var postcssDiscardDuplicates = require('postcss-discard-duplicates');
 
-gulp.task('sass', function () {
-  gulp.src('scss/style.scss')
+gulp.task('sass-bootstrap', function () {
+  gulp.src('scss/bootstrap/bootstrap.scss')
     .pipe(bulk())
     .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-    .pipe(rename(function (path) {
-      path.basename = 'bootstrap-civicrm-' + path.basename;
-    }))
     .pipe(gulp.dest('css/'));
 });
 
-gulp.task('global-sass', function () {
-  gulp.src('scss/global-theme-overrides/global-custom.scss')
+gulp.task('sass-civicrm', function () {
+  gulp.src('scss/civicrm/civicrm.scss')
     .pipe(bulk())
-    .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
-    .pipe(rename(function (path) {
-      path.basename = 'global-custom';
-    }))
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
     .pipe(postcss([postcssPrefix({
       prefix: '.crm-container ',
-      exclude: [/ui-dialog/, /ui-datepicker/, /select2/, /logged-in/, /cke/, // absolute positioned elements
-        /^\.crm-container/] // avoid repeating the namespace
+      exclude: [/page-civicrm/, /crm-container/]
     }), postcssDiscardDuplicates]))
     .pipe(gulp.dest('css/'));
 });
+
+gulp.task('sass', ['sass-bootstrap', 'sass-civicrm']);
 
 gulp.task('watch', function () {
   gulp.watch('scss/**/*.scss', ['sass']);
