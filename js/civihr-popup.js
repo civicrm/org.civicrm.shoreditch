e6d8276ac53dbd2d41c1313de6f2bd99ca71e40c
Krzysztof Kałamarski <kkalamarski@gmail.com>
2016-10-25 12:35:32 +0300

    PCHR-1566: Moved $.fn.attrchange to hrui extention

--- js/civihr-popup.js
+++ js/civihr-popup.js
@@ -4,29 +4,6 @@ Prevent Popups to overflow tables.
 CRM.$(function($) {
   'use strict';
 
-  (function($) {
-    var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
-
-    $.fn.attrchange = function(callback) {
-      if (MutationObserver) {
-        var options = {
-          subtree: false,
-          attributes: true
-        };
-
-        var observer = new MutationObserver(function(mutations) {
-          mutations.forEach(function(e) {
-            callback.call(e.target, e);
-          });
-        });
-
-        return this.each(function() {
-          observer.observe(this, options);
-        });
-      }
-    }
-  })($);
-
   // .btn-slide is a continer of popup and a button triggering it
   $('.btn-slide').attrchange(function(e) {
     var $button = $(this),
