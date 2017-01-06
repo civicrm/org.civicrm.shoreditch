f3892ffef8db6c45b4aa1fe88a9bbeade4d9d8e0
Buddha Man Nepali <bmnepali980@gmail.com>
2017-01-06 08:22:32 +0545

    PCHR-1720: Extracted styles form PCHR-1353 branch

--- gulpfile.js
+++ gulpfile.js
@@ -2,31 +2,36 @@ var gulp = require('gulp');
 var bulk = require('gulp-sass-bulk-import');
 var sass = require('gulp-sass');
 var postcss = require('gulp-postcss');
-var stripCssComments = require('gulp-strip-css-comments');
-// The "postcss-prefix-selector" uses ES6 promises
-require('es6-promise').polyfill();
-// Used to add necessary prefixes at the css level
 var postcssPrefix = require('postcss-prefix-selector');
-// Used to remove duplicated css rules
 var postcssDiscardDuplicates = require('postcss-discard-duplicates');
+var stripCssComments = require('gulp-strip-css-comments');
+var transformSelectors = require("gulp-transform-selectors");
 
-gulp.task('sass-bootstrap', function (next) {
+var bootstrapNamespace = '#bootstrap-theme';
+
+gulp.task('sass-bootstrap', function () {
   gulp.src('scss/bootstrap/bootstrap.scss')
     .pipe(bulk())
     .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
     .pipe(stripCssComments({ preserve: false }))
-    .pipe(gulp.dest('css/')).on('end', next);
+    .pipe(postcss([postcssPrefix({
+      prefix: bootstrapNamespace + ' ',
+      exclude: [/^html/, /^body/]
+    })]))
+    .pipe(transformSelectors(namespaceRootElements, { splitOnCommas: true }))
+    .pipe(gulp.dest('css/'));
 });
 
-gulp.task('sass-civicrm', function (next) {
+gulp.task('sass-civicrm', function () {
   gulp.src('scss/civicrm/custom-civicrm.scss')
     .pipe(bulk())
     .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(stripCssComments({ preserve: false }))
     .pipe(postcss([postcssPrefix({
       prefix: '.crm-container ',
       exclude: [/page-civicrm/, /crm-container/]
     }), postcssDiscardDuplicates]))
-    .pipe(gulp.dest('css/')).on('end', next);
+    .pipe(gulp.dest('css/'));
 });
 
 gulp.task('sass', ['sass-bootstrap', 'sass-civicrm']);
@@ -36,3 +41,21 @@ gulp.task('watch', function () {
 });
 
 gulp.task('default', ['sass', 'watch']);
+
+/**
+ * Apply the namespace on html and body elements
+ *
+ * @param  {string} selector the current selector to be transformed
+ * @return string
+ */
+function namespaceRootElements(selector) {
+  var regex = /^(body|html)/;
+
+  if (regex.test(selector)) {
+    selector = selector.replace(regex, function (match) {
+      return match + bootstrapNamespace;
+    }) + ",\n" + selector.replace(regex, bootstrapNamespace);
+  }
+
+  return selector;
+}
