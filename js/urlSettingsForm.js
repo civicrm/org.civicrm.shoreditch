e615293dd233900dd92317a6a984c1f12f74c253
Coleman Watts <coleman@civicrm.org>
2017-04-06 23:24:03 -0400

    Add help text for custom css gui

--- js/urlSettingsForm.js
+++ js/urlSettingsForm.js
@@ -7,6 +7,7 @@ CRM.$(function($) {
     .css('width', '100%')
     .wrap('<div style="width:41.4em; display: flex;" />')
     .prop('readonly', url === val)
+    .prop('placeholder', ts('Default CiviCRM style'))
     .before('<div style="white-space: nowrap;"><input type="radio" name="shoreditch-toggle" id="shoreditch-toggle-1" ' + (url === val ? 'checked' : '') + ' /> <label for="shoreditch-toggle-1">Shoreditch</label>' +
       ' &nbsp; <input type="radio" name="shoreditch-toggle" id="shoreditch-toggle-0" ' + (url !== val ? 'checked' : '') + ' /> <label for="shoreditch-toggle-0">' + ts('Other:') + '</label>&nbsp;</div>')
     .parent()
@@ -17,4 +18,11 @@ CRM.$(function($) {
         $('#customCSSURL').val('').prop('readonly', false);
       }
     });
+  // Replace help text with our own
+  $('.crm-url-form-block-customCSSURL .helpicon').removeAttr('onclick').click(function() {
+    CRM.help($('label[for=customCSSURL]').text(), '<p>' +
+      ts('Enabling Shoreditch CSS will change the look and feel of CiviCRM sitewide to give the entire application a consistent modern style.') +
+      '</p>');
+    return false;
+  });
 });
