4105ce2fd1c890022de5338397981b5ad3829ac8
Mukesh Ram <mukesh@compucorp.co.uk>
2017-10-26 14:55:44 +0530

    AI-160: Putting code under documented function

--- js/crm-ui.js
+++ js/crm-ui.js
@@ -3,12 +3,20 @@
  */
 
 (function ($, _) {  
-  $(document).ready(function () {
+
+  /**
+  * Substitutes main menu arrow image with FontAwesome caret icon
+  */
+  function substituteMenuArrows() {
     $('#root-menu-div .menu-item-arrow').each(function ($element) {
       var $arrow = $(this);
 
       $arrow.before('<i class="fa fa-caret-right menu-item-arrow"></i>');
       $arrow.remove();
     });
+  }
+
+  $(document).ready(function () {
+    substituteMenuArrows();
   });
 }(CRM.$, CRM._));
