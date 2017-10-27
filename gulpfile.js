9caa1d6d9b1c14edb25a4b5a3a67a2316ecdce3b
René Olivo <minet.ws@gmail.com>
2017-10-27 08:11:40 -0400

    PCHR-2714: Fix ui bootstrap tooltips attached to the body

--- gulpfile.js
+++ gulpfile.js
@@ -38,7 +38,7 @@ gulp.task('sass:civicrm', ['sass:sync'], function () {
     .pipe(stripCssComments({ preserve: false }))
     .pipe(postcss([postcssPrefix({
       prefix: '.crm-container ',
-      exclude: [/page-civicrm/, /crm-container/, /civicrm-menu/, /#root-menu-div/]
+      exclude: [/tooltip/, /page-civicrm/, /crm-container/, /civicrm-menu/, /#root-menu-div/]
     }), postcssDiscardDuplicates]))
     .pipe(gulp.dest('css/'));
 });
