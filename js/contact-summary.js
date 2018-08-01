80c6228df22e27d7ca32aa6a3c2ece9cba21d6db
Swastik Pareek <swastik1608@gmail.com>
2018-08-01 15:19:17 +0530

    SHOR-22: Add contact picture to contact summary page for shoreditch theme for civiCRM

---
+++ js/contact-summary.js
@@ -0,0 +1,15 @@
+/* global CRM */
+(function ($) {
+  $(document).ready(function () {
+    moveUserPictureNextToContactTitle();
+  });
+
+  /**
+   * Deletes User Image parent div (#crm-contact-thumbnail) and move the user picture next to contatct tile.
+   * Also adds class "crm-summary-contactphoto" to it.
+   */
+  function moveUserPictureNextToContactTitle () {
+    $('#contactname-block').prepend($('#crm-contact-thumbnail').find('img').addClass('crm-summary-contactphoto'));
+    $('#crm-contact-thumbnail').remove();
+  }
+}(CRM.$));
