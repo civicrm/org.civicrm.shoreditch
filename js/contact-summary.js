b875ae2f701f9aa6b6830bc6cef3ca575a76b843
Swastik Pareek <swastik1608@gmail.com>
2018-08-02 13:44:52 +0530

    SHOR-22: PR feedback - Fix typo in function description

--- js/contact-summary.js
+++ js/contact-summary.js
@@ -4,7 +4,7 @@
   });
 
   /**
-   * Moves image el left to the Contact title on contact Summary page
+   * Moves the user image to the left of the contact name
    */
   function moveUserPictureNextToContactTitle () {
     $('.crm-summary-contactname-block').prepend($('#crm-contact-thumbnail'));
