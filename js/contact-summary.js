3e3a566fb882e1105813c7f8f8dd4f8ccb7c42ca
Alessandro Verdura <alessandro@compucorp.co.uk>
2018-09-11 15:32:25 +0200

    SHOR-71: Make the profile pic a background image via javascript

--- js/contact-summary.js
+++ js/contact-summary.js
@@ -1,5 +1,8 @@
 (function ($) {
+  var profilePicWrapper = '#crm-contact-thumbnail';
+
   $(document).ready(function () {
+    usePictureAsBackground();
     moveUserPictureNextToContactTitle();
   });
 
@@ -7,6 +10,22 @@
    * Moves the user image to the left of the contact name
    */
   function moveUserPictureNextToContactTitle () {
-    $('.crm-summary-contactname-block').prepend($('#crm-contact-thumbnail'));
+    $('.crm-summary-contactname-block').prepend($(profilePicWrapper));
+  }
+
+  /**
+   * Makes the user picture the background image of the <a>
+   * element that links to the picture itself
+   *
+   * Having it as a background allows for support of different
+   * picture ratios that would otherwise get stretched with a <img> element
+   */
+  function usePictureAsBackground () {
+    var $wrapper = $(profilePicWrapper);
+    var $picture = $wrapper.find('.crm-contact_image');
+    var pictureSrc = $picture.find('img').attr('src');
+
+    $picture.find('a').css('background-image', 'url(' + pictureSrc + ')');
+    $picture.find('img').remove();
   }
 }(CRM.$));
