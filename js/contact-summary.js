9bbf477071420ba3bc9cf7fc43448e18847f27f3
Alessandro Verdura <alessandro@compucorp.co.uk>
2018-09-13 08:52:43 +0200

    SHOR-71: Improve profile pic manipulation logic

--- js/contact-summary.js
+++ js/contact-summary.js
@@ -1,35 +1,44 @@
 (function () {
-  var profilePicWrapper = '#crm-contact-thumbnail';
-
   document.addEventListener('DOMContentLoaded', function () {
-    usePictureAsBackground();
-    moveUserPictureNextToContactTitle();
+    manipulateProfilePicture();
   });
 
   /**
-   * Moves the user image to the left of the contact name
+   * Performs DOM manipulation on the profile picture element, if present
    */
-  function moveUserPictureNextToContactTitle () {
-    var header = document.querySelector('.crm-summary-contactname-block');
-    var wrapper = document.querySelector(profilePicWrapper);
-    var block = document.querySelector('.crm-summary-block');
+  function manipulateProfilePicture () {
+    var picWrapper = document.querySelector('#crm-contact-thumbnail');
 
-    header.insertBefore(wrapper, block);
-  }
+    if (!picWrapper) {
+      return;
+    }
 
-  /**
-   * Makes the user picture the background image of the <a>
-   * element that links to the picture itself
-   *
-   * Having it as a background allows for support of different
-   * picture ratios that would otherwise get stretched with a <img> element
-   */
-  function usePictureAsBackground () {
-    var wrapper = document.querySelector(profilePicWrapper);
-    var picture = wrapper.querySelector('.crm-contact_image');
-    var src = picture.querySelector('img').getAttribute('src');
+    usePictureAsBackground();
+    moveUserPictureNextToContactTitle();
 
-    picture.querySelector('a').style.backgroundImage = 'url(' + src + ')';
-    picture.querySelector('a').removeChild(picture.querySelector('img'));
+    /**
+     * Moves the picture to the left of the contact name
+     */
+    function moveUserPictureNextToContactTitle () {
+      var header = document.querySelector('.crm-summary-contactname-block');
+      var block = document.querySelector('.crm-summary-block');
+
+      header.insertBefore(picWrapper, block);
+    }
+
+    /**
+     * Makes the picture the background image of the <a>
+     * element that links to the picture itself
+     *
+     * Having it as a background allows for support of different
+     * picture ratios that would otherwise get stretched with a <img> element
+     */
+    function usePictureAsBackground () {
+      var picture = picWrapper.querySelector('.crm-contact_image');
+      var src = picture.querySelector('img').getAttribute('src');
+
+      picture.querySelector('a').style.backgroundImage = 'url(' + src + ')';
+      picture.querySelector('a').removeChild(picture.querySelector('img'));
+    }
   }
 }());
