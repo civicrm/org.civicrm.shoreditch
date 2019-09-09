23b733d7f291c4e55e34c1813242f31fa00066df
Vinu Varshith <vinu@compucorp.co.uk>
2019-09-09 17:24:37 +0530

    SHOR-155: Enable Shoreditch As Backend Theme On Install

--- shoreditch.php
+++ shoreditch.php
@@ -28,6 +28,20 @@ function shoreditch_civicrm_xmlMenu(&$files) {
  */
 function shoreditch_civicrm_install() {
   _shoreditch_civix_civicrm_install();
+
+  // Unset "custom css url" (customCSSURL) field
+  $customCSSUrl = Civi::settings()->get("customCSSURL");
+  if (strpos($customCSSUrl, 'org.civicrm.shoreditch') !== FALSE) {
+    civicrm_api3('setting', 'create', [
+      'customCSSURL' => ''
+    ]);
+  }
+
+  // Set "shoreditch" as the backend theme.
+  civicrm_api3('setting', 'create', [
+    'theme_frontend' => 'default',
+    'theme_backend' => 'shoreditch'
+  ]);
 }
 
 /**
